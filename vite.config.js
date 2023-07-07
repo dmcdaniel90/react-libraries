30457a8147d54379ca434768a8719b46bb37bff8
dmcdaniel90 <dmcdaniel9@gmail.com>
2023-07-07 11:59:48 +0100

    Updated svgs, .gitignore, vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,16 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import svgr from 'vite-plugin-svgr';
+import { copy } from 'vite-plugin-copy';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/react-libraries',
-  plugins: [react()],
+  plugins: [
+    react(),
+    svgr(),
+    copy({
+      targets: [{ src: 'src/assets', dest: 'dist/assets' }],
+    }),
+  ],
 });
